1438f6b3eda067d51904f23fc75d213d0de0eae6
Mihir <mihir@mihir.work>
2023-05-14 11:35:27 +0530

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,34 @@
-import { cleanup, render, screen } from '@testing-library/react'
-import { afterEach } from 'vitest'
-
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
-
-afterEach(() => {
-  cleanup()
-})
 
 describe('renders', () => {
   it('title', () => {
     render(<App />)
-
-    expect(screen.getByText(/Rekenmachine/i)).toBeInTheDocument()
+    expect(screen.getByText(/rekenmachine/i)).toBeInTheDocument()
   })
+})
+
+function calculate(buttons: string[], expectedValue: string) {
+  it(`${buttons.join(' ')} ${expectedValue}`, async () => {
+    render(<App />)
+
+    for (const button of buttons) {
+      await userEvent.click(screen.getByTestId(`button-${button}`))
+    }
+
+    expect(screen.getByTestId('display')).toHaveTextContent(expectedValue)
+  })
+}
+
+describe('returns correct result for', () => {
+  calculate(['2', '+', '1', '='], '3')
+
+  calculate(['3', '-', '1', '='], '2')
+
+  calculate(['5', 'x', '2', '='], '10')
+
+  calculate(['6', '÷', '2', '='], '3')
+
+  calculate(['2', '+', '1', 'AC'], '0')
 })
